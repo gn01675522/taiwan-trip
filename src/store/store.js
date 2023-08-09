7b16f535278b0978bda72181b10ab3768b889ccc
gn01675522 <gn01675522@gmail.com>
2023-08-09 22:45:29 +0800

    fix: Fixed the issue where the logger would still display in the production environment.“

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,14 @@ import logger from "redux-logger";
 import { rootReducer } from "./rootReducer";
 import thunk from "redux-thunk";
 
+const middlewares = [
+  process.env.NODE_ENV === "development" && logger,
+  thunk,
+].filter(Boolean);
+
 export const store = configureStore({
   reducer: rootReducer,
   preloadedState: undefined,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(
-      [process.env.NODE_ENV !== "production" && thunk, logger].filter(Boolean)
-    ),
+    getDefaultMiddleware().concat(middlewares),
 });
